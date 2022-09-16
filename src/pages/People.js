35ae9cd134f96ec72b327b6517ed071db552718e
ekelly55 <eamonn.michael.kellyr@email.com>
2022-09-16 11:04:20 -0400

    edit route working

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const People = (props) => {
     // state to hold formData
-    
+
     const createPeople = async (personData) => {
         try {
             // make post request to create people
@@ -16,29 +16,29 @@ const People = (props) => {
                 body: JSON.stringify(personData),
             }
             );
-       
+
             // testing API create request
             // console.log(await newPerson.json())
-            
+
             // trigger fetch of updated People to replace stale content
             getPeople()
-            
+
         } catch (err) {
             console.log(err)
-            
+
         }
         console.log(personData)
         //undefined. it's not grabbing person data from the form...how is it supposed to?
     };
-    
+
     const [newForm, setNewForm] = useState({
         name: "",
         image: "",
         title: "",
     });
-    
+
     const BASE_URL = "https://people-backend-lab.herokuapp.com/people";
-    
+
     // handleChange function for form
     const handleChange = (e) => {
         setNewForm({ ...newForm, [e.target.name]: e.target.value });
@@ -47,16 +47,16 @@ const People = (props) => {
 
         e.preventDefault()
         const newPerson = await createPeople(newForm)
-            //something is missing here. e.target?
-            // reset the form
-            console.log(e.target)
+        //something is missing here. e.target?
+        // reset the form
+        console.log(e.target)
         setNewForm({ name: "", image: "", title: "" })
 
     }
-    
+
     const [people, setPeople] = useState([])
-    
-    
+
+
     const getPeople = async () => {
         try {
             const response = await fetch(BASE_URL)
@@ -78,9 +78,9 @@ const People = (props) => {
                 <div key={person._id} className='person-card'>
                     <Link to={`/people/${person._id}`}>
 
-                    <h1>{person.name}</h1>
-                    <img src={person.image} />
-                    <h3>{person.title}</h3>
+                        <h1>{person.name}</h1>
+                        <img src={person.image} />
+                        <h3>{person.title}</h3>
                     </Link>
                 </div>
 
@@ -103,7 +103,7 @@ const People = (props) => {
     );
 
     return (
-        <section className="people-list">
+        <section className='personIndexMain'>
             <div className="addNewForm">
                 <h2>Create a new person</h2>
                 <form onSubmit={handleSubmit}>
@@ -132,10 +132,13 @@ const People = (props) => {
                 </form>
             </div>
 
+            <section className="people-list">
 
-            {people && people.length ? loaded() : loading()
-            //what does this line mean? if people and people length exist, then set loaded() as loading?
-            }</section>
+                {people && people.length ? loaded() : loading()
+                    //what does this line mean? if people and people length exist, then set loaded() as loading?
+                }</section>
+
+        </section>
     );
 
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,46 +1,145 @@
 import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 
 const Show = () => {
-  const [person, setPerson] = useState(null)
-  const { id } = useParams()
-  const URL = `https://people-backend-lab.herokuapp.com/people/${id}`
-
-  const getPerson = async () => {
-    try {
-      const response = await fetch(URL)
-      const result = await response.json()
-      setPerson(result)
-    } catch (err) {
-      console.log(err)
+    const [person, setPerson] = useState(null)
+    const { id } = useParams()
+    const URL = `https://people-backend-lab.herokuapp.com/people/${id}`
+    
+    const getPerson = async () => {
+        try {
+            const response = await fetch(URL)
+            const result = await response.json()
+            setPerson(result)
+        } catch (err) {
+            console.log(err)
+        }
     }
-  }
+    
+    console.log(`Current Person: ${JSON.stringify(person)}`)
+    
+    useEffect(() => {
+        getPerson()
+    }, [])
+    
+    const navigate = useNavigate()
+    
+    const [editForm, setEditForm] = useState(null)
 
-  console.log(`Current Person: ${JSON.stringify(person)}`)
+    const handleChange = event => {
+          setEditForm({ ...editForm, [event.target.name]: event.target.value })
+    }
 
-  useEffect(() => {
-    getPerson()
-  }, [])
+    const handleSubmit = async (e) => {
+        console.log("submit fired")
+        e.preventDefault()
+        const options = {
+            method: "PUT",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(editForm)
+        }
+        try {
+            const response = await fetch(URL, options)
+            const updatedPerson = await response.json()
+            setPerson(updatedPerson)
+            setEditForm(updatedPerson)
 
-// After testing that person is defined you can use a ternary statement to
-// conditionally render either a person's data or a loading message
+        } catch(err) {
+            console.log(err)
+        }
+    }
 
-const loaded = () => (
-    <div className="person">
-        <h1>Show Page</h1>
-        <h2>{person.name}</h2>
-        <h2>{person.title}</h2>
-        <img src={person.image} alt={person.name+" image"} />
-</div>
-)
+    const removePerson = async () => {
+        try {
+            const options = {
+                method: "DELETE"
+            }
+    
+            const response = await fetch(URL, options)
+    
+            // you can inspect the response for debugging or extended 
+            //functionality. 
+            const deletedPerson = await response.json()
+    
+            // console.log(deletedPerson)
+            navigate('/')
+    
+            // navigate will change the browser's URL
+            // which will cause react-router to "redirect" to home page;
+            // the Main will then re-render the People component
+            // upon mount People will fetch the updated index of people data
+    
+        } catch (err) {
+            console.log(err)
+            navigate(URL)
+        }
+    }
+    
+    
+    const updatePerson = async (e) => {
+        e.preventDefault()
+    
+        // make put request to update a person
+        try {
+          await fetch(URL, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(editForm),
+          })
+    
+          // trigger a re-render after the fetch is complete
+          getPerson()
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    
 
-const loading = () => {
-    return <h1>Loading.........</h1>
-            // alternatively you can use the spinner 
-}
 
-return people ? loaded() : loading()
 
+
+    // After testing that person is defined you can use a ternary statement to
+    // conditionally render either a person's data or a loading message
+
+    const loaded = () => (
+        <div className="person">
+            <section>
+	<h2>Edit this Person</h2>
+    <form onSubmit={handleSubmit}>
+    <input
+        type="text"
+        value= {editForm.name}
+        name="name"
+        placeholder="name"
+        onChange={handleChange}
+      />
+     <button clasName="edit" onClick={handleSubmit}>Edit Person</button>
+  </form>
+</section>
+            <h1>{person.name}</h1>
+            <h2>{person.title}</h2>
+            <img src={person.image} alt={person.name + " image"} />
+            <div>
+                <button className="delete" onClick={removePerson}>
+                    Remove Person
+                </button>
+            </div>
+            
+        </div>
+    )
+
+    const loading = () => {
+        return <h1>Loading.........</h1>
+        // alternatively you can use the spinner 
+    }
+
+    
+
+    return person ? loaded() : loading()
 
 }
 
